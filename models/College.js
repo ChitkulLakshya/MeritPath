cd163f0a21ab2bce043ecf8b317c01c07d47525f
ChitkulLakshya <Chitkullakshya@gmail.com>
2025-11-28 14:01:59 +0530

    changed college.js

--- models/College.js
+++ models/College.js
@@ -10,27 +10,53 @@ const collegeSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
+
   course: {
     type: String,
     required: true,
-    enum: ['CSE', 'ECE', 'MECH'],
+    enum: [
+      'CSE',
+      'ECE',
+      'MECH',
+      'EEE',
+      'CIVIL',
+      'IT',
+      'BIOTECH',
+      'CHEM',
+      'PETRO',
+      'ROBOTICS',
+      'AI',
+      'DS',
+      'CYBER',
+      'IOT',
+      'AGRI',
+      'AUTO',
+      'MINING',
+      'METALLURGY',
+      'BIO_MED',
+      'ENV'
+    ],
     uppercase: true
   },
+
   mcet_rank_cutoff: {
     type: Number,
     required: true,
     min: 0
   },
+
   intermediate_marks_cutoff: {
     type: Number,
     required: true,
     min: 0,
     max: 100
   },
+
   fees: {
     type: Number,
     min: 0
   },
+
   affiliation: {
     type: String,
     trim: true
@@ -39,8 +65,11 @@ const collegeSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Index for efficient querying
-collegeSchema.index({ course: 1, mcet_rank_cutoff: 1, intermediate_marks_cutoff: 1 });
+// Index for faster course-based searches
+collegeSchema.index({
+  course: 1,
+  mcet_rank_cutoff: 1,
+  intermediate_marks_cutoff: 1
+});
 
 module.exports = mongoose.model('College', collegeSchema);
-
